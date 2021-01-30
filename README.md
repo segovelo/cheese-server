63198853b0575502d2f0daf6f513ae85017d3e5c
segovelo <gauchosinpingo@gmail.com>
2021-01-30 21:02:41 +0000

    Further description on README.md and change to local.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cheeses-server
-Express server hosting 460  cheese descriptions
+Express server hosting 466  cheese descriptions in JSON format. To run locally:
+
+node local.js
+
+Listen on port 5000 --> http://localhost:5000/cheeses
--- local.js
+++ local.js
@@ -1,4 +1,4 @@
 const app = require('./server');
 // run the server locally
-app.listen(5000, () => console.log('Server listening at http://localhost:5000'))
+app.listen(5000, () => console.log('Server listening at http://localhost:5000/cheeses'))
 
